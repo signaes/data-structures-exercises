5d9e1cf2e756bc4c92006a4b53c564e8219769d1
Thiago Oliveira <thiago@signaes.com>
2019-02-18 06:55:58 -0300

    add implementation with channel

---
+++ queue/channels/main.go
@@ -0,0 +1,28 @@
+package main
+
+type Queue struct {
+	items chan int
+}
+
+func (q *Queue) Enqueue(item int) {
+	q.items <- item
+}
+
+func (q *Queue) Dequeue() int {
+	return <-q.items
+}
+
+func main() {
+	q := Queue{
+		items: make(chan int, 16),
+	}
+	q.Enqueue(1)
+	q.Enqueue(9)
+	q.Enqueue(2)
+	q.Enqueue(80)
+
+	println(q.Dequeue())
+	println(q.Dequeue())
+	println(q.Dequeue())
+	println(q.Dequeue())
+}
